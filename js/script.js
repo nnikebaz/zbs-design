49717533eb9a708848e52cefea1b2da5f6cf8d9b
Nnikebaz <zabekin.nikita@gmail.com>
2024-07-08 14:14:25 +0200

    Чуть всё не поломалось

---
+++ js/script.js
@@ -0,0 +1,166 @@
+$(document).ready(function () {
+  const mediaQuery = {
+    xs: window.matchMedia("(max-width:  575.98px)"),
+    sm: window.matchMedia("(max-width:  767.98px)"),
+    md: window.matchMedia("(max-width:  991.98px)"),
+    lg: window.matchMedia("(max-width:  1199.98px)"),
+    xl: window.matchMedia("(max-width:  1399.98px)"),
+    big: window.matchMedia("(min-width:  991.98px)"),
+  };
+
+  function stepSelect() {
+    const stepButtons = document.querySelectorAll(".way-section__left-name");
+    const dropdownToggle = document.querySelector(".dropdown-toggle");
+
+    const changeBorder = (btn) => {
+      if (mediaQuery.big.matches) {
+        stepButtons.forEach((item) => item.classList.remove("active"));
+        btn.classList.add("active");
+      }
+    };
+    const makeActiveContent = (btn) => {
+      const prevActiveItem = document.querySelector(".way-section__right.active");
+      if (prevActiveItem) {
+        prevActiveItem.classList.remove("active");
+      }
+      const dataTargetItem = btn.getAttribute("data-target");
+      const contentItem = document.getElementById(dataTargetItem);
+      contentItem.classList.add("active");
+    };
+    const updateDropdownText = (btn) => {
+      dropdownToggle.textContent = btn.textContent;
+    };
+
+    stepButtons.forEach(btn => btn.addEventListener('click', function(event) {
+      event.preventDefault()
+
+      changeBorder(btn)
+      makeActiveContent(btn)
+      updateDropdownText(btn)
+    }))
+
+  }
+
+  function noUpdateSite() {
+    $("form").submit(function (event) {
+      event.preventDefault();
+    });
+  }
+
+  function validate() {
+    function maskNumber() {
+      $("#phone").mask("+7(000)000-00-00");
+    }
+
+    maskNumber();
+
+    $(".validate").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+        },
+        phone: {
+          required: true,
+          minlength: 16,
+        },
+      },
+      messages: {
+        name: "Пожалуйста, заполните своё имя.",
+        phone: "Пожалуйста, заполните корректно номер телефона.",
+      },
+      highlight: function (element) {
+        $(element).addClass("inputError");
+      },
+      unHighlight: function (element) {
+        $(element).removeClass("inputError");
+      },
+    });
+
+    $("#btn-form").click(function () {
+      console.log($("form").valid());
+    });
+  }
+
+  function Smooth() {
+    SmoothScroll({
+      // Время скрола 400 = 0.4 секунды
+      animationTime: 400,
+      // Размер шага в пикселях
+      stepSize: 75,
+
+      // Дополнительные настройки:
+
+      // Ускорение
+      accelerationDelta: 30,
+      // Максимальное ускорение
+      accelerationMax: 2,
+
+      // Поддержка клавиатуры
+      keyboardSupport: true,
+      // Шаг скролла стрелками на клавиатуре в пикселях
+      arrowScroll: 50,
+
+      // Pulse (less tweakAble)
+      // ratio of "tail" to "acceleration"
+      pulseAlgorithm: true,
+      pulseScale: 4,
+      pulseNormalize: 1,
+
+      // Поддержка тачпада
+      touchpadSupport: true,
+    });
+  }
+
+  function dragEllipse() {
+    let parallaxScene1 = document.getElementById("parallax-scene-1");
+    let parallaxInstance1 = new Parallax(parallaxScene1, {
+      limitX: false,
+      limitY: false,
+      scalarX: 20,
+      scalarY: 60,
+      invertX: false,
+      invertY: false,
+    });
+    let parallaxScene2 = document.getElementById("parallax-scene-2");
+    let parallaxInstance2 = new Parallax(parallaxScene2, {
+      limitX: 150,
+      limitY: false,
+      scalarX: 5,
+      scalarY: 20,
+      invertX: false,
+      invertY: false,
+    });
+  }
+
+  function makeDropdown() {
+    const dropdown = document.querySelector(".way-section__left");
+    const dropdownMenu = document.querySelector(".tabs");
+    const dropdownItem = document.querySelectorAll(".way-section__left-name");
+
+    const addClassForDropdown = () => {
+      if (mediaQuery.xs.matches) {
+        dropdown.classList.add("dropdown");
+        dropdownMenu.classList.add("dropdown-menu");
+        dropdownItem.forEach((element) =>
+          element.classList.add("dropdown-item")
+        );
+      } else {
+        dropdown.classList.remove("dropdown");
+        dropdownMenu.classList.remove("dropdown-menu");
+        dropdownItem.forEach((element) =>
+          element.classList.remove("dropdown-item")
+        );
+      }
+    };
+    addClassForDropdown(mediaQuery);
+    mediaQuery.xs.addEventListener("change", addClassForDropdown);
+  }
+
+  makeDropdown();
+  stepSelect();
+  noUpdateSite();
+  validate();
+  Smooth();
+  dragEllipse();
+});
