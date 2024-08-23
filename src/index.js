6411686b5646f34096e49f959a4451e93b4d3d26
Nnikebaz <zabekin.nikita@gmail.com>
2024-08-23 14:35:26 +0200

    23.08 webp format convert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import manifest from './manifest.webmanifest';
-import './sass/style.sass';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+// import css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './sass/style.sass';
 
 // import js
 import './js/script.js';
 import './js/itc-slider.js';
 
-// import css
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css';
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,117 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const loader = require("sass-loader");
 
 module.exports = {
-  entry: './src/index.js', // Основная точка входа для JavaScript
+  entry: "./src/index.js", // Основная точка входа для JavaScript
 
   output: {
-    filename: 'bundle.js', // Имя выходного файла сборки
-    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+    filename: "bundle.js", // Имя выходного файла сборки
+    path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
     clean: true, // Очищает папку dist перед новой сборкой
   },
 
   module: {
     rules: [
       {
-        test: /\.webmanifest$/,  // Указываем расширение файла
-        type: 'asset/resource',  // Обрабатываем файл как ресурс (то есть он просто копируется в выходную папку)
+        test: /\.webmanifest$/, // Указываем расширение файла
+        type: "asset/resource", // Обрабатываем файл как ресурс (то есть он просто копируется в выходную папку)
         generator: {
-          filename: '[name][ext]',  // Имя файла в выходной папке (dist)
+          filename: "[name][ext]", // Имя файла в выходной папке (dist)
         },
       },
       {
         test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
-        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Лоадеры для обработки CSS
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Лоадеры для обработки CSS
       },
       {
         test: /\.s[ac]ss$/i, // Обработка файлов .sass и .scss
         use: [
           MiniCssExtractPlugin.loader, // Извлекает CSS в отдельные файлы
-          'css-loader', // Обрабатывает CSS
-          'sass-loader' // Обрабатывает SASS/SCSS
+          "css-loader", // Обрабатывает CSS
+          "sass-loader", // Обрабатывает SASS/SCSS
         ],
       },
       {
         test: /\.(js|jsx)$/, // Регулярное выражение для обработки JavaScript и JSX
         exclude: /node_modules/, // Игнорировать файлы из папки node_modules
         use: {
-          loader: 'babel-loader', // Лоадер для трансформации JavaScript с помощью Babel
+          loader: "babel-loader", // Лоадер для трансформации JavaScript с помощью Babel
           options: {
-            presets: ['@babel/preset-env'], // Пресет Babel для работы с современным JavaScript
+            presets: ["@babel/preset-env"], // Пресет Babel для работы с современным JavaScript
           },
         },
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i, // Регулярное выражение для обработки изображений
-        type: 'asset/resource', // Используем встроенный механизм Webpack для обработки изображений
+        test: /\.(png|jpg|jpeg)$/i, // Регулярное выражение для обработки изображений
+        type: "asset/resource", // Используем встроенный механизм Webpack для обработки изображений
         generator: {
-          filename: 'assets/images/[name][ext]', // Путь и имя файла для изображений в выходной папке dist
+          filename: "img/[name][ext]", // Путь и имя файла для изображений в выходной папке dist
         },
+      },
+      {
+        test: /\.(`|svg|ico)$/i, // Регулярное выражение для обработки изображений
+        type: "asset/resource", // Используем встроенный механизм Webpack для обработки изображений
+        generator: {
+          filename: "icons/[name][ext]", // Путь и имя файла для изображений в выходной папке dist
+        },
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
       },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Шаблон для генерации index.html в папке dist
+      template: "./src/index.html", // Шаблон для генерации index.html в папке dist
+      minify: {
+        html5                          : true,
+        collapseWhitespace             : true,
+        minifyCSS                      : true,
+        minifyJS                       : true,
+        minifyURLs                     : false,
+        removeAttributeQuotes          : true,
+        removeComments                 : true, // false for Vue SSR to find app placeholder
+        removeEmptyAttributes          : true,
+        removeOptionalTags             : true,
+        removeRedundantAttributes      : true,
+        useShortDoctype                : true
+      }
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/style.css', // Путь и имя файла для CSS
+      filename: "css/style.css", // Путь и имя файла для CSS
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'static/img'), // Копируем изображения
-          to: path.resolve(__dirname, 'dist/img'), // Путь в dist, куда будут копироваться изображения
+          from: path.resolve(__dirname, "src/manifest.webmanifest"), // Копируем манифест
+          to: path.resolve(__dirname, "dist/manifest.webmanifest"), // Путь в dist манифест
         },
-        {
-          from: path.resolve(__dirname, 'static/icons'), // Копируем иконки
-          to: path.resolve(__dirname, 'dist/icons'), // Путь в dist, куда будут копироваться иконки
+      ],
+    }),
+    new ImageMinimizerPlugin({
+      minimizer: {
+        implementation: ImageMinimizerPlugin.imageminGenerate,
+        options: {
+          plugins: [
+            ["imagemin-webp", { quality: 90 }], // Настройка качества для WebP
+          ],
         },
+      },
+      generator: [
         {
-          from: path.resolve(__dirname, 'src/manifest.webmanifest'), // Копируем манифест
-          to: path.resolve(__dirname, 'dist/manifest.webmanifest'), // Путь в dist манифест
+          // Генерация WebP файлов
+          preset: "webp",
+          implementation: ImageMinimizerPlugin.imageminGenerate,
+          options: {
+            plugins: [["imagemin-webp", { quality: 90 }]],
+          },
+          filename: "img/[name].webp", // Имя файла для WebP в выходной папке
         },
       ],
     }),
@@ -80,12 +119,12 @@ module.exports = {
 
   devServer: {
     static: {
-      directory: path.join(__dirname, 'static'), // Каталог для статики
+      directory: path.join(__dirname, "dist"), // Каталог для статики
     },
     open: true, // Автоматически открывать браузер
     port: 9000, // Задаем порт для devServer
-    watchFiles: ['src/**/*', 'static/**/*'],
+    watchFiles: ["src/**/*", "static/**/*"],
   },
 
-  mode: 'development', // Режим сборки
+  mode: "development", // Режим сборки
 };
